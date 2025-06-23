5a1d8b143c0b4d7e676192c2edda5f809a910a24
Dhruv <dbpthk@gmail.com>
2025-06-23 17:52:46 +1000

    mobile navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,29 @@
+import { useState } from "react";
 import { assets } from "../assets/frontend_assets/assets";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [visible, setvisible] = useState(false);
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="brand logo" />
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          <hr className="w-3/4 border-none h-[1.5px] bg-gray-300 hidden" />
         </NavLink>
         <NavLink to="/collection" className="flex flex-col items-center gap-1">
           <p>COLLECTION</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          <hr className="w-3/4 border-none h-[1.5px] bg-gray-300 hidden" />
         </NavLink>
         <NavLink to="/about" className="flex flex-col items-center gap-1">
           <p>ABOUT</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          <hr className="w-3/4 border-none h-[1.5px] bg-gray-300 hidden" />
         </NavLink>
         <NavLink to="/contact" className="flex flex-col items-center gap-1">
           <p>CONTACT</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          <hr className="w-3/4 border-none h-[1.5px] bg-gray-300 hidden" />
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
@@ -49,6 +52,60 @@ const Navbar = () => {
             10
           </p>
         </Link>
+        <img
+          onClick={() => setvisible(true)}
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+          alt="mobile menu icon"
+        />
+      </div>
+      {/* sidebar menu for small screen */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setvisible(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img
+              className=" h-4 rotate-180"
+              src={assets.dropdown_icon}
+              alt="menu go back icon"
+            />
+            <p>Back</p>
+          </div>
+          <NavLink
+            onClick={() => setvisible(false)}
+            className="py-3 pl-6 border-b border-gray-300"
+            to="/"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            onClick={() => setvisible(false)}
+            className="py-3 pl-6 border-b border-gray-300"
+            to="/collection"
+          >
+            COLLECTION
+          </NavLink>
+          <NavLink
+            onClick={() => setvisible(false)}
+            className="py-3 pl-6 border-b border-gray-300"
+            to="/about"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            onClick={() => setvisible(false)}
+            className="py-3 pl-6  border-b border-gray-300"
+            to="/contact"
+          >
+            CONTACT
+          </NavLink>
+        </div>
       </div>
     </div>
   );
